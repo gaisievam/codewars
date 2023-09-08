4ad2d51a132e56d91395f081422d385c51153f34
gaisievam <gaisieva.m@mail.ru>
2023-09-08 22:37:32 +0500

    17

---
+++ 8kyu/print-a-rectangle-using-asterisks.js
@@ -0,0 +1,25 @@
+//DESCRIPTION:
+// Write a method that, given two arguments, width and height, returns a string representing a rectangle with those dimensions.
+
+// The rectangle should be filled with spaces, and its borders should be composed of asterisks (*).
+
+// For example, the following call:
+
+// getRectangleString(3, 3);
+// Should return the following string:
+
+// ***
+// * *
+// ***
+// End each line of the string (including the last one) with a carriage return-line feed combination.
+
+// Note: You may assume that width and height will always be greater than zero.
+
+
+//SOLUTION:
+function getRectangleString(w,h) {
+    const rn = '\r\n';
+    const tb = '*'.repeat(w)+rn;
+    const center = ( w > 1 )?('*'+' '.repeat(w-2)+'*'+rn).repeat(h-2):'';
+    return h > 1 ? (tb + center + tb) : tb.repeat(h);
+   }
